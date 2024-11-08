7a6749fb0722ceeab3ff92ba3e6ffc2cabd30724
David Hajdarovic <david.hajdarovic@boxmark.com>
2024-11-08 14:39:25 +0100

    footer design done

--- resources/views/components/footer/phone-footer.blade.php
+++ resources/views/components/footer/phone-footer.blade.php
@@ -1,12 +1,12 @@
 <div>
     <footer class="footer fixed-bottom bg-black py-2 px-3">
             <div class="d-flex justify-content-between">
-                <div class="text-success align-content-center">
+                <div class="text-white align-content-center">
                     {{ Auth::user()->name }}
                 </div>
                 <div class="">
                     <button class="btn btn-dark btn-sm"><i class="bi bi-chat"></i><span class="badge text-danger">2</span></button>
-                    <button class="btn btn-dark btn-sm"><i class="bi bi-controller bg-dark"></i><span class="badge text-danger">19</span></button>
+                    <button class="btn btn-dark btn-sm"><i class="bi bi-controller"></i><span class="badge text-danger">19</span></button>
                 </div>
                 
 

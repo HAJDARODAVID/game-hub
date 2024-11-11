032d887b6645b9d76bed6e1a8c83b1a1db7861d4
David Hajdarovic <david.hajdarovic@boxmark.com>
2024-11-11 14:37:32 +0100

    need to push for live preview

--- resources/views/app/game/game-info.blade.php
+++ resources/views/app/game/game-info.blade.php
@@ -30,6 +30,28 @@
                     <div class="col-md">
                         <x-cards.basic-card classAtt='mb-2'> 
                             <x-slot:title>open games</x-slot:title>
+                            <div class="card bg-dark" style="border-radius: 0px !important;">
+                                <div class="card-body text-white py-2">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="">
+                                            <b>Game by: david69</b> <br>
+                                            <span class="text-danger">Players is name: 12</span>
+                                        </div><br>
+                                        <div class=""><button class="btn btn-success btn-sm" style="border-radius: 0px !important;">Join</button></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card bg-dark" style="border-radius: 0px !important;">
+                                <div class="card-body text-white py-2">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="">
+                                            <b>Game by: david69</b> <br>
+                                            <span class="text-danger">Players is name: 12</span>
+                                        </div><br>
+                                        <div class=""><button class="btn btn-success btn-sm" style="border-radius: 0px !important;">Join</button></div>
+                                    </div>
+                                </div>
+                            </div>
                         </x-cards.basic-card>
                     </div>
                 </div>

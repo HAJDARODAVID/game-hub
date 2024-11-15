571ea73d3975d559491d8da711b28e632b42ea3a
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-11-15 08:44:42 +0100

    testing live

--- resources/views/livewire/messenger/messenger-conversation.blade.php
+++ resources/views/livewire/messenger/messenger-conversation.blade.php
@@ -1,6 +1,6 @@
 <div style="display: flex;
     flex-direction: column;
-    height: 70vh;">
+    height: 60vh;">
     <div class="d-flex align-items-end" style="flex: 1;">
         <div >...</div>
     </div>

2fb751f41442ddafaef46870aed0c46914f2c1fd
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-11-15 09:12:40 +0100

    stil testing live

--- resources/views/livewire/messenger/messenger-conversation.blade.php
+++ resources/views/livewire/messenger/messenger-conversation.blade.php
@@ -1,7 +1,37 @@
 <div style="display: flex; flex-direction: column; height: 65vh;">
     <div class="d-flex align-items-start flex-column" style="height: 200%;">
-        <div class="">Flex itex itex itex itex itex itex itex itex itex itex itex itex itex itex itex item</div>
-        <div class="">Flex item</div>
+        <div class="w-100">
+            <x-cards.item-card>
+                IME ČOVEKA
+            </x-cards.item-card>
+        </div>
+        <div class="w-100 overflow-auto py-2" style="height: 48vh;">
+            Flex item <br>
+            Flex item <br>Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+            Flex item <br>
+        </div>
         <div class="mt-auto w-100">
             <div class="form-group">
                 <textarea class="form-control" style="border-radius: 0px !important"></textarea>

bfa44a29fd4eea3427fb70c1c4c6ae295fb12d65
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-11-15 09:05:22 +0100

    testing live

--- resources/views/livewire/messenger/messenger-conversation.blade.php
+++ resources/views/livewire/messenger/messenger-conversation.blade.php
@@ -1,7 +1,11 @@
 <div style="display: flex; flex-direction: column; height: 65vh;">
-    <div class="d-flex align-items-end flex-column" style="height: 200%;">
-        <div class="p-2">Flex item</div>
-        <div class="p-2">Flex item</div>
-        <div class="mt-auto p-2">Flex item</div>
+    <div class="d-flex align-items-start flex-column" style="height: 200%;">
+        <div class="">Flex itex itex itex itex itex itex itex itex itex itex itex itex itex itex itex item</div>
+        <div class="">Flex item</div>
+        <div class="mt-auto w-100">
+            <div class="form-group">
+                <textarea class="form-control" style="border-radius: 0px !important"></textarea>
+            </div>
+        </div>
       </div>
 </div>

5320a67a9c320d67dddb04d7a936c93a1ae6c979
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-12-16 10:57:35 +0100

    i dont know

--- resources/views/livewire/messenger/messenger-conversation.blade.php
+++ resources/views/livewire/messenger/messenger-conversation.blade.php
@@ -10,7 +10,7 @@
         </div>
         <div class="w-100" x-data="{ messages: '{{ $messages }}' }" wire:poll.5s='setMessages'> {{-- wire:poll.5s='setMessages' --}}
             <div>
-                <div id="messeges" class="overflow-auto my-2 p-1" style="height: 48vh; width:100%" x-ref="scrollableDiv" x-init="$refs.scrollableDiv.scrollTop = ($refs.scrollableDiv.scrollHeight)+999">
+                <div id="messeges" class="overflow-auto my-2 p-1" style="height: 48vh; width:100%" x-ref="scrollableDiv" x-init="$refs.scrollableDiv.scrollTop = ($refs.scrollableDiv.scrollHeight+999)">
                     @foreach ($messages as $msg)
                         @livewire('messenger.message-container', ['msg' => $msg], key($msg->id . now()))   
                     @endforeach
@@ -20,7 +20,7 @@
         <div class="mt-auto w-100">
             <div class="form-group d-flex align-items-center gap-2 mt-2">
                 <textarea class="form-control" style="border-radius: 0px !important" wire:model.blur='newMessage'></textarea>
-                <button class="btn btn-dark shadow" style="border-radius: 0px !important;" wire:click='sendNewMessage()' x-on:click="$refs.scrollableDiv.scrollTop = $refs.scrollableDiv.scrollHeight"><i class="bi bi-send"></i></button>
+                <button class="btn btn-dark shadow" style="border-radius: 0px !important;" wire:click='sendNewMessage()' x-on:click="$refs.scrollableDiv.scrollTop = ($refs.scrollableDiv.scrollHeight+999)"><i class="bi bi-send"></i></button>
             </div>
         </div>
       </div>

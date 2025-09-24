91fcaf5214682ab65168881e36caa390a9d775cb
David Hajdarovic <hajdarovicdavid@gmail.com>
2025-09-24 12:44:01 +0200

    removed unecesery

--- resources/views/components/game-controller/toe-tac-tic/player-header-info.blade.php
+++ resources/views/components/game-controller/toe-tac-tic/player-header-info.blade.php
@@ -1,12 +1,4 @@
 <div class="row">
-    {{-- <div class="col">
-        <div class="d-flex justify-content-center"><p class="h3">X</p></div>
-        <div class="d-flex justify-content-center">{{ Auth::user()->name }}</div>
-    </div>
-    <div class="col">
-        <div class="d-flex justify-content-center"><p class="h3">O</p></div>
-        <div class="d-flex justify-content-center">{{ Auth::user()->name }}</div>
-    </div> --}}
     @foreach ($playersInGame as $playerID => $player)
         <div class="col @if (in_array($playerID, $currentPlayersId)) text-success @endif">
             <div class="d-flex justify-content-center"><p class="h3"><i class="{{ $player['game_piece'] }}"></i></p></div>
